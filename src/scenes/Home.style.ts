83e153777a9b6dbe7d3b97068b0da7058774ce7f
Andy Wooller <andy@wooller.london>
2020-07-02 18:41:45 +0200

    basic text

---
+++ src/scenes/Home.style.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components/native';
+
+export const ClockDetails = styled.View`
+  align-items: center;
+`;
+
+export const LocationText = styled.Text`
+  font-size: 30px;
+  color: #d81f72;
+  font-weight: 500;
+`;
+
+export const DigitalTimeText = styled.Text`
+  font-size: 47px;
+  color: black;
+  font-weight: 500;
+`;
--- src/scenes/Home.tsx
+++ src/scenes/Home.tsx
@@ -2,10 +2,16 @@ import React from 'react';
 import { View } from 'react-native';
 import Clock from '../components/Clock/Clock';
 
+import { ClockDetails, LocationText, DigitalTimeText } from './Home.style';
+
 export const HomeScene = () => {
   return (
     <View>
       <Clock />
+      <ClockDetails>
+        <LocationText>Genova</LocationText>
+        <DigitalTimeText>18:30</DigitalTimeText>
+      </ClockDetails>
     </View>
   );
 };
