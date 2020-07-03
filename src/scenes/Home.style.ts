1c5b320dbdbe8a95d65566ad19b348dcd653fa56
Andy Wooller <andy@wooller.london>
2020-07-03 12:28:06 +0200

    spacing

--- src/scenes/Home.style.ts
+++ src/scenes/Home.style.ts
@@ -2,12 +2,14 @@ import styled from 'styled-components/native';
 
 export const ClockDetails = styled.View`
   align-items: center;
+  margin-top: 70px;
 `;
 
 export const LocationText = styled.Text`
   font-size: 30px;
   color: #d81f72;
   font-weight: 500;
+  margin-bottom: 15px;
 `;
 
 export const DigitalTimeText = styled.Text`
